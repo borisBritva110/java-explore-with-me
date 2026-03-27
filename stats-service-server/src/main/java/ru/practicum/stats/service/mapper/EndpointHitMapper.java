150fc4c0946188493d7c1b12e5f1eae39bca7157
m.imaeva <m.imaeva@vk.team>
2026-03-27 15:45:47 +0300

    test stat svc

--- stats-service-server/src/main/java/ru/practicum/stats/service/mapper/EndpointHitMapper.java
+++ stats-service-server/src/main/java/ru/practicum/stats/service/mapper/EndpointHitMapper.java
@@ -4,7 +4,6 @@ import ru.practicum.stats.dto.EndpointHitDto;
 import ru.practicum.stats.service.model.EndpointHit;
 
 public class EndpointHitMapper {
-    
     public static EndpointHit toEntity(EndpointHitDto dto) {
         EndpointHit entity = new EndpointHit();
         entity.setApp(dto.getApp());
@@ -12,15 +11,5 @@ public class EndpointHitMapper {
         entity.setIp(dto.getIp());
         entity.setTimestamp(dto.getTimestamp());
         return entity;
-    }
-    
-    public static EndpointHitDto toDto(EndpointHit entity) {
-        EndpointHitDto dto = new EndpointHitDto();
-        dto.setId(entity.getId());
-        dto.setApp(entity.getApp());
-        dto.setUri(entity.getUri());
-        dto.setIp(entity.getIp());
-        dto.setTimestamp(entity.getTimestamp());
-        return dto;
     }
 }
