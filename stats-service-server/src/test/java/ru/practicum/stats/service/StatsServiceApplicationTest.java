ad2a2c040b2ffcceb2be7b03671da6645fae0058
m.imaeva <m.imaeva@vk.team>
2026-03-27 14:38:30 +0300

    test stat svc

---
+++ stats-service-server/src/test/java/ru/practicum/stats/service/StatsServiceApplicationTest.java
@@ -0,0 +1,13 @@
+package ru.practicum.stats.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StatsServiceApplicationTest {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
