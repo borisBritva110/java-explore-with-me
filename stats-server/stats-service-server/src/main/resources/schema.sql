b44edb95cd6ad2b7f47f29dd231d2576d1a1ba56
m.imaeva <m.imaeva@vk.team>
2026-03-29 21:45:44 +0300

    test stat svc

--- stats-server/stats-service-server/src/main/resources/schema.sql
+++ stats-server/stats-service-server/src/main/resources/schema.sql
@@ -6,5 +6,5 @@ CREATE TABLE IF NOT EXISTS hits
     timestamp TIMESTAMP WITHOUT TIME ZONE
     );
 
-CREATE INDEX IF NOT EXISTS idx_endpoint_hit_timestamp ON endpoint_hits(timestamp);
-CREATE INDEX IF NOT EXISTS idx_endpoint_hit_uri ON endpoint_hits(uri);
+CREATE INDEX IF NOT EXISTS idx_endpoint_hit_timestamp ON hits(timestamp);
+CREATE INDEX IF NOT EXISTS idx_endpoint_hit_uri ON hits(uri);
