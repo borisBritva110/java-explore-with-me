6bddd6335dce1af62566d7aa96f856d29006c7cd
m.imaeva <m.imaeva@vk.team>
2026-04-05 16:43:22 +0300

    test main svc

--- stats-server/stats-service-server/src/main/java/ru/practicum/stats/service/service/StatsService.java
+++ stats-server/stats-service-server/src/main/java/ru/practicum/stats/service/service/StatsService.java
@@ -1,6 +1,7 @@
 package ru.practicum.stats.service.service;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import ru.practicum.stats.dto.EndpointHitDto;
@@ -15,17 +16,21 @@ import static java.util.Objects.nonNull;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 @Transactional
 public class StatsService {
     private final StatsRepository statsRepository;
 
     public void hit(EndpointHitDto hitDto) {
+        log.info("Saving hit for URI: {}, IP: {}, App: {}", hitDto.getUri(), hitDto.getIp(), hitDto.getApp());
         EndpointHit hit = EndpointHitMapper.toEntity(hitDto);
-        statsRepository.save(hit);
+        EndpointHit savedHit = statsRepository.save(hit);
+        log.info("Hit saved with ID: {}", savedHit.getId());
     }
 
     public List<ViewStatsDto> getStats(LocalDateTime start, LocalDateTime end, List<String> uris, Boolean unique) {
-        return switch (unique != null ? (unique ? 1 : 2) : 3) {
+        log.info("Getting stats - start: {}, end: {}, uris: {}, unique: {}", start, end, uris, unique);
+        List<ViewStatsDto> result = switch (unique != null ? (unique ? 1 : 2) : 3) {
             case 1 -> nonNull(uris)
                 ? statsRepository.findUniqueWithUrisStats(uris, start, end)
                 : statsRepository.findUniqueAndNoUrisStats(start, end);
@@ -34,5 +39,10 @@ public class StatsService {
                 : statsRepository.findNoUniqueAndNoUrisStats(start, end);
             default -> throw new IllegalArgumentException("Unique parameter cannot be null");
         };
+
+        log.info("Found {} stats records", result.size());
+        result.forEach(stat -> log.info("Stat: {} - {} hits", stat.getUri(), stat.getHits()));
+
+        return result;
     }
 }
