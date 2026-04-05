e4121ba14040aa78d83aaf967d52ed25a85620af
m.imaeva <m.imaeva@vk.team>
2026-04-05 03:39:36 +0300

    test main svc

--- stats-server/stats-service-dto/src/main/java/ru/practicum/stats/dto/EndpointHitDto.java
+++ stats-server/stats-service-dto/src/main/java/ru/practicum/stats/dto/EndpointHitDto.java
@@ -1,6 +1,10 @@
 package ru.practicum.stats.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Past;
@@ -10,16 +14,21 @@ import java.time.LocalDateTime;
 import com.fasterxml.jackson.annotation.JsonFormat;
 
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 public class EndpointHitDto {
-    private Long id;
-    @NotBlank
+
+    @NotBlank(message = "Идентификатор сервиса не может быть пустым")
     private String app;
-    @NotBlank
+
+    @NotBlank(message = "URI не может быть пустым")
     private String uri;
-    @NotBlank
+
+    @NotBlank(message = "IP-адресс не может быт пустым")
     private String ip;
-    @Past
-    @NotNull
-    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+
+    @NotNull(message = "Временная отметка запроса к эндпоинту не может быть пустой")
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss")
     private LocalDateTime timestamp;
 }
